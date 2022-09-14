4cd5875f84d2d538f85a2cde09307b352992af26
mosquito0076 <hsmk0076@naver.com>
2022-09-14 17:08:55 +0900

    [FE] 수정 : 부트스트랩 업데이트
    Former-commit-id: 9d26037d3de8420178275ee0b92a87cc901b5cc5

--- front-end/src/game/Game.js
+++ front-end/src/game/Game.js
@@ -1,9 +1,14 @@
+import styles from './css/Game.module.css';
+import bgimg from '../assets/background1.jpg'
 
 const Game = () => {
 
   return (
     <>
-      <p>Game</p>
+      <img src={bgimg} alt='bg' className={styles.bg}/>
+      <div className={`${styles['box']} justify-self-center mx-auto`}>
+        <div className={`${styles['innerbox']}`}></div> 
+      </div>
     </>
   );
 };
