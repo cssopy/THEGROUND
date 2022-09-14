14b5b7e11c8b014d1437d246babf940fbc24d462
jihyeon3560 <jihyeon3560@naver.com>
2022-09-14 17:53:11 +0900

    S07P22D109-100 [FE] 생성: 영상 자동재생
    Former-commit-id: 49f1ac5e8b8a9c0b8d639b195e86c531b7ff8029

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Game from './game/Game';
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Game from "./game/Game";
+import Landing from "./landing/Landing";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Landing />} />
       <Route path="/game" element={<Game />} />
     </Routes>
   );
