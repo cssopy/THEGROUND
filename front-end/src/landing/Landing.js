7b9a9d746b130f76f458460529d3fa1773f0aad9
jihyeon3560 <jihyeon3560@naver.com>
2022-09-15 14:35:37 +0900

    S07P22D109-94 [FE] 수정: 랜딩페이지 디자인
    Former-commit-id: 2a710fb1ba48ff6a2237c9354c92579798589d68

--- front-end/src/landing/Landing.js
+++ front-end/src/landing/Landing.js
@@ -1,11 +1,29 @@
 import styles from "./css/Landing.module.css";
 import landingVideo from "../assets/landing-dark.mp4";
+import title from "../assets/title.png";
+import subtitle from "../assets/subtitle.png";
 
 const Landing = () => {
   return (
-    <video autoPlay muted loop className={styles.video}>
-      <source src={landingVideo} type="video/mp4" />
-    </video>
+    <>
+      <video autoPlay muted loop className={styles.bg}>
+        <source src={landingVideo} type="video/mp4" />
+      </video>
+      <div>
+        <div className={styles.title}>
+          <img src={title} alt="" />
+        </div>
+        <div className={styles.subtitle}>
+          <img src={subtitle} alt="" />
+        </div>
+      </div>
+      <div className={styles.buttonLoc}>
+        <button className={styles.button}>Sign Up</button>
+      </div>
+      <div className={styles.buttonLoc}>
+        <button className={styles.button}>Login</button>
+      </div>
+    </>
   );
 };
 
