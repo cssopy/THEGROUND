c98e1b64ad6dd28920b54c54dd85de90381c5b83
kofgb <kofgb1234@naver.com>
2022-09-15 20:46:34 +0900

    [BE] 기타 : CI/CD 작동 확인을 위한 테스트 메서드 작성

--- back-end/src/main/java/com/ssafy/theground/controller/UserController.java
+++ back-end/src/main/java/com/ssafy/theground/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.ssafy.theground.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+	@GetMapping("")
+	public String testMethod() {
+		return "hello~";
+	}
 }
