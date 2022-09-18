b22dbcb4d0d95e094959da0fbfde5a35ceacbef1
mosquito0076 <hsmk0076@naver.com>
2022-09-18 22:51:44 +0900

    [FE] 수정 : table module css 수정
    Former-commit-id: 50b644a18775dd3d2b23d9d7ca09fad7a75bcaf3

--- front-end/src/game/components/StrikeZone.js
+++ front-end/src/game/components/StrikeZone.js
@@ -1,12 +1,33 @@
 import styles from '../css/StrikeZone.module.css';
+import Table from 'react-bootstrap/esm/Table';
 
 const StrikeZone = (props) => {
   const { balls } = props;
 
   return (
     <>
-      <div className={`${styles['strike-zone']}`} >
-        공 날아오는거 찍기
+      <div className={`${styles['strike-zone']} d-flex justify-content-center align-items-center`} >
+        <div className={`${styles.mainbox}`}>
+          <Table bordered>
+            <tbody>
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
     </>
   );
