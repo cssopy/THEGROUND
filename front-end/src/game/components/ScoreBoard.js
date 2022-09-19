ab5520c030fa53eec7bc3ad8302171b1c5216bb9
mosquito0076 <hsmk0076@naver.com>
2022-09-19 19:45:00 +0900

    [FE] 생성: 전광판, 스트라이크 존 생성
    Former-commit-id: 9bff8e1bd9ab6bf0069909e15d14cb1bb1f853a6

--- front-end/src/game/components/ScoreBoard.js
+++ front-end/src/game/components/ScoreBoard.js
@@ -5,6 +5,16 @@ const ScoreBoard = (props) => {
 
   const { scores, R, H, B } = props;
 
+  const score = (num) => {
+    if (num < scores.length) {
+      if (num === scores.length-1) {
+        return <div style={{color: '#BF0D3E'}}>{scores[num]}</div>
+      }
+      return <div>{scores[num]}</div>;
+    } else {
+      return <div></div>
+    }
+  }
 
   return (
     <>
@@ -12,15 +22,15 @@ const ScoreBoard = (props) => {
         <thead className={styles.thead}>
           <tr className={styles.top}>
             <th className={styles.left}></th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
+            <th className={styles.green}>1</th>
+            <th className={styles.green}>2</th>
+            <th className={styles.green}>3</th>
+            <th className={styles.green}>4</th>
+            <th className={styles.green}>5</th>
+            <th className={styles.green}>6</th>
+            <th className={styles.green}>7</th>
+            <th className={styles.green}>8</th>
+            <th className={styles.green}>9</th>
             <th>R</th>
             <th>H</th>
             <th className={styles.right}>B</th>
@@ -29,33 +39,33 @@ const ScoreBoard = (props) => {
         <tbody className={styles.tbody}>
           <tr>
             <th className={styles.left}>logo</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th className={styles.right}></th>
+            <th>{score(0)}</th>
+            <th>{score(2)}</th>
+            <th>{score(4)}</th>
+            <th>{score(6)}</th>
+            <th>{score(8)}</th>
+            <th>{score(10)}</th>
+            <th>{score(12)}</th>
+            <th>{score(14)}</th>
+            <th>{score(16)}</th>
+            <th>{R[0]}</th>
+            <th>{H[0]}</th>
+            <th className={styles.right}>{B[0]}</th>
           </tr>          
           <tr className={styles.bottom}>
             <th className={styles.left}>logo</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th className={styles.right}></th>
+            <th>{score(1)}</th>
+            <th>{score(3)}</th>
+            <th>{score(5)}</th>
+            <th>{score(7)}</th>
+            <th>{score(9)}</th>
+            <th>{score(11)}</th>
+            <th>{score(13)}</th>
+            <th>{score(15)}</th>
+            <th>{score(17)}</th>
+            <th>{R[1]}</th>
+            <th>{H[1]}</th>
+            <th className={styles.right}>{B[1]}</th>
           </tr>
         </tbody>
       </Table>
