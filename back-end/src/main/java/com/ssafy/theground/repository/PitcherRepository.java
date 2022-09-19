088ea2fc87bda8f3419564ef2493bc13b92201bf
Juwon-Kiim <sa6812000@naver.com>
2022-09-19 21:08:19 +0900

    [BE] 수정 : 미보유 투수 목록 상세 조회 수정

--- back-end/src/main/java/com/ssafy/theground/repository/PitcherRepository.java
+++ back-end/src/main/java/com/ssafy/theground/repository/PitcherRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.theground.repository;
 import com.ssafy.theground.entity.Pitcher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PitcherRepository extends JpaRepository<Pitcher, Long> {
+    List<Pitcher> findAllByIdNotIn(List<Long> pitcherSeq);
 }
