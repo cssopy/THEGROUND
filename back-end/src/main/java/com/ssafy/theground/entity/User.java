6c56fc4dd4c4a4dade164e3b31f41d7cefc9e130
Juwon-Kiim <sa6812000@naver.com>
2022-09-20 14:27:08 +0900

    [BE] 수정 : 구원투수 목록 상세 조회 수정

--- back-end/src/main/java/com/ssafy/theground/entity/User.java
+++ back-end/src/main/java/com/ssafy/theground/entity/User.java
@@ -1,13 +1,6 @@
 package com.ssafy.theground.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -57,4 +50,7 @@ public class User {
 	
 	@Column(name="user_lose")
 	private int userLose;
+
+	@OneToOne(mappedBy = "userSeq")
+	private TeamSetting teamSetting;
 }
