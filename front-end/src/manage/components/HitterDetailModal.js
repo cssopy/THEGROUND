a2fde7a3e27b65ce388e8fad7cc04e7fce4107c9
cssopy <83584721+cssopy@users.noreply.github.com>
2022-09-21 11:33:11 +0900

    [FE] 생성, 수정 : 구단 관리 페이지
    드래그 앤 드랍 마무리
    구단 관리 페이지 스타일 수정
    
    
    Former-commit-id: a7a0081133bff9e4db09285bc7e495c5e085bb7b

---
+++ front-end/src/manage/components/HitterDetailModal.js
@@ -0,0 +1,46 @@
+import { Row, Table } from "react-bootstrap";
+import Radar from "./Radar";
+
+import style from "../css/HitterDetailModal.module.css";
+
+const HitterDetailModal = (props) => {
+  const data = [
+    {
+      taste: "fruity",
+      chardonay: 30,
+    },
+    {
+      taste: "bitter",
+      chardonay: 41,
+    },
+    {
+      taste: "heavy",
+      chardonay: 20,
+    },
+    {
+      taste: "strong",
+      chardonay: 114,
+    },
+    {
+      taste: "sunny",
+      chardonay: 82,
+    },
+  ];
+  return (
+    <>
+      <Row className={style["modal"]}>
+        <Row className={style["head"]}>
+          <div>{props.hitter.name}</div>
+        </Row>
+        <Row className={style["body"]}>
+          <Row>
+            <Radar data={data} />
+          </Row>
+          <Row className={style["stat"]}></Row>
+        </Row>
+      </Row>
+    </>
+  );
+};
+
+export default HitterDetailModal;
