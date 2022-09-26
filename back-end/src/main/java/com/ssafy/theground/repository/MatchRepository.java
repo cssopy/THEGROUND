5f2f30d488b716e2ea61b4edf4306332976779a0
Juwon-Kiim <sa6812000@naver.com>
2022-09-26 13:30:38 +0900

    [BE] 추가 : entity 추가

---
+++ back-end/src/main/java/com/ssafy/theground/repository/MatchRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.theground.repository;
+
+import com.ssafy.theground.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findTop3ByUserSeq(Long userSeq);
+}
--- back-end/src/main/java/com/ssafy/theground/service/GameService.java
+++ back-end/src/main/java/com/ssafy/theground/service/GameService.java
@@ -1,13 +1,32 @@
 package com.ssafy.theground.service;
 
+import com.ssafy.theground.entity.Match;
+import com.ssafy.theground.entity.User;
+import com.ssafy.theground.repository.MatchRepository;
+import com.ssafy.theground.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
 
-    public void teamBriefInfo(){
+    private final UserRepository userRepository;
 
+    private final JwtService jwtService;
+
+    private final MatchRepository matchRepository;
+
+    public void teamBriefInfo() throws Exception {
+        Optional<User> byUserUid = userRepository.findByUserUid(jwtService.getUserUid(jwtService.getJwt()));
+        if(byUserUid.isPresent()){
+            List<Match> top3ByUserSeq = matchRepository.findTop3ByUserSeq(byUserUid.get().getUserSeq());
+
+        }
     }
 }
