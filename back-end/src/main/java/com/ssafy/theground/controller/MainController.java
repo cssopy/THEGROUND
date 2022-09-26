38db32271b70393e05400320b83269f13f006d96
Juwon-Kiim <sa6812000@naver.com>
2022-09-26 16:31:45 +0900

    [BE] 추가 : 경기 일ì정 api 추가

--- back-end/src/main/java/com/ssafy/theground/controller/MainController.java
+++ back-end/src/main/java/com/ssafy/theground/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.ssafy.theground.controller;
 
+import com.ssafy.theground.service.MainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/main")
+@RequiredArgsConstructor
 public class MainController {
+
+    private final MainService mainService;
 
     @GetMapping("/schedules")
     public ResponseEntity<?> getSchedules() throws Exception {
-        return new ResponseEntity<>(true, HttpStatus.OK);
+        if(mainService.getSchedules() != null) {
+            return new ResponseEntity<>(mainService.getSchedules(), HttpStatus.OK);
+        }
+        else return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 }
--- back-end/src/main/java/com/ssafy/theground/service/MainService.java
+++ back-end/src/main/java/com/ssafy/theground/service/MainService.java
@@ -1,8 +1,17 @@
 package com.ssafy.theground.service;
 
-import com.ssafy.theground.repository.UserRepository;
+import com.ssafy.theground.dto.res.MatchResDto;
+import com.ssafy.theground.entity.AITeam;
+import com.ssafy.theground.entity.Schedule;
+import com.ssafy.theground.entity.Season;
+import com.ssafy.theground.entity.User;
+import com.ssafy.theground.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,14 +19,34 @@ public class MainService {
 
     private final UserRepository userRepository;
 
+    private final SeasonRepository seasonRepository;
+
+    private final ScheduleRepository scheduleRepository;
+
+    private final AITeamRepository aiTeamRepository;
+
+    private final LogoRepository logoRepository;
+
     private final JwtService jwtService;
 
-//    public List<MatchResDto> getSchedules() throws Exception {
-//        Optional<User> byUserUid = userRepository.findByUserUid(jwtService.getUserUid(jwtService.getJwt()));
-//
-//        if(byUserUid.isPresent()){
-//            Season byUserSeq = seasonRepository.findByUserSeq(byUserUid.get().getUserSeq());
-//        }
-//
-//    }
+    public List<MatchResDto> getSchedules() throws Exception {
+        Optional<User> byUserUid = userRepository.findByUserUid(jwtService.getUserUid(jwtService.getJwt()));
+        List<MatchResDto> list = new ArrayList<>();
+
+        if(byUserUid.isPresent()){
+            Season byUserSeq = seasonRepository.findByUserSeq(byUserUid.get().getUserSeq());
+            List<Schedule> byScheduleSeq = scheduleRepository.findTop3ByScheduleSeq(byUserSeq.getSeasonSeq());
+            for(Schedule schedule : byScheduleSeq){
+                MatchResDto matchResDto = new MatchResDto();
+                AITeam byAiTeamSeq = aiTeamRepository.findByAiTeamSeq(schedule.getTeamSeq().getAiTeamSeq());
+
+                matchResDto.setTeamName(byAiTeamSeq.getAiTeamName());
+                matchResDto.setLogoUrl(logoRepository.findByLogoSeq(byAiTeamSeq.getLogoSeq()).getLogoUrl());
+
+                list.add(matchResDto);
+            }
+            return list;
+        }
+        else return null;
+    }
 }
