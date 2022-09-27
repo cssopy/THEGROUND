d37e114f4b0a89a250b58e2975ac68519245ef2a
mosquito0076 <hsmk0076@naver.com>
2022-09-28 00:13:09 +0900

    [FE] 수정 : 회원가입 수정
    Former-commit-id: 496a701d99f9a3757d4b165b4931a5590a2decc4

--- front-end/src/landing/components/Kakao.js
+++ front-end/src/landing/components/Kakao.js
@@ -2,8 +2,8 @@ import styles from "../css/Modal.module.css";
 
 const Kakao = () => {
   const REST_API_KEY = "1ae04a78365d2a5f1e2e1d4ee529fe84";
-  const REDIRECT_URI = "https://j7d109.p.ssafy.io";
-  // const REDIRECT_URI = "http://localhost:3000";
+  // const REDIRECT_URI = "https://j7d109.p.ssafy.io";
+  const REDIRECT_URI = "http://localhost:3000";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const kakaoLogin = () => {
