200ca23f2c84c8e3bcbcf83714a24fde4476b6aa
cssopy <83584721+cssopy@users.noreply.github.com>
2022-09-28 15:04:19 +0900

    [FE] 생성 : 게임 상대 매칭
    Former-commit-id: d20261fa9973198fdd3793fe837cf6b2b2e5418d

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ import SignupModal from "./landing/components/SignupModal";
 import Manage from "./manage/Manage";
 import Market from "./market/Market";
 import Game from "./game/Game";
+import MatchOPPO from "./game/MatchOPPO";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Route path="/manage" element={<Manage />}></Route>
       <Route path="/market" element={<Market />}></Route>
       <Route path="/game" element={<Game />}></Route>
+      <Route path="/match" element={<MatchOPPO />}></Route>
     </Routes>
   );
 }
