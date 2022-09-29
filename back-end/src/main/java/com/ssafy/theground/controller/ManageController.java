2d1a978f64110649e14e5036e56157d93f2c818a
kofgb <kofgb1234@naver.com>
2022-09-29 14:46:42 +0900

    S07P22D109-217 [BE] 완성 : 선발 로테이션 수정 기능 완성

--- back-end/src/main/java/com/ssafy/theground/controller/ManageController.java
+++ back-end/src/main/java/com/ssafy/theground/controller/ManageController.java
@@ -59,16 +59,31 @@ public class ManageController {
 			String uid = jwtService.getUserUid(jwtService.getJwt());
 			Long[] seq = new Long[5];
 			seq[0] = vo.get("teamSetting1stSp");
-			seq[1] = vo.get("teamSettingwndSp");
+			seq[1] = vo.get("teamSetting2ndSp");
 			seq[2] = vo.get("teamSetting3rdSp");
 			seq[3] = vo.get("teamSetting4thSp");
 			seq[4] = vo.get("teamSetting5thSp");
+			
+			//중복으로 들어온 선수가 있는지 확인. 중복이 있을 시 
+			for(int i=0;i<4;i++) {
+				for(int j=i+1;j<5;j++) {
+					if(seq[i] == seq[j]) {
+						resultMap.put("message", "업데이트에 실패하였습니다.");
+						status = HttpStatus.BAD_REQUEST;
+						return new ResponseEntity<>(resultMap, status);
+					}
+				}
+			}
+			
+			manageService.rotationUpdate(uid, seq);
+			
+			resultMap.put("message", "업데이트 성공.");
+			status = HttpStatus.ACCEPTED;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		for(int i=0;i<5;i++) {
-			
+			resultMap.put("message", "업데이트에 실패하였습니다.");
+			status = HttpStatus.BAD_REQUEST;
 		}
 		
 		return new ResponseEntity<>(resultMap, status);
--- back-end/src/main/java/com/ssafy/theground/service/ManageService.java
+++ back-end/src/main/java/com/ssafy/theground/service/ManageService.java
@@ -113,16 +113,29 @@ public class ManageService {
     	return result;
     }
     
-    public void rotationUpdate(String uid, Long[] list) {
+    public void rotationUpdate(String uid, Long[] list) throws Exception {
     	Optional<UserPitcher> o;
+    	User u = userRepository.findByUserUid(uid).get();
+    	TeamSetting t = teamSettingRepository.findByUserSeq_UserSeq(u.getUserSeq());
     	Long userSeq = userRepository.findByUserUid(uid).get().getUserSeq();
     	for(int i=0;i<5;i++) {
     		o = userPitcherRepository.findByUserSeq_UserSeqAndPitcherSeq(userSeq, list[i]);
     		if(o.isPresent()) {
-    			
+    			if(i==0) {
+    				t.setTeamSetting1stSp(o.get());
+    			}else if(i==1) {
+    				t.setTeamSetting2ndSp(o.get());
+    			}else if(i==2) {
+    				t.setTeamSetting3rdSp(o.get());
+    			}else if(i==3) {
+    				t.setTeamSetting4thSp(o.get());
+    			}else if(i==4) {
+    				t.setTeamSetting5thSp(o.get());
+    			}
     		}else {
-    			return;
+    			throw new Exception("에러 발생");
     		}
     	}
+    	teamSettingRepository.save(t);
     }
 }
