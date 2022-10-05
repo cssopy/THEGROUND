042964a893908e364def181702737d04d0dbe46f
mosquito0076 <hsmk0076@naver.com>
2022-10-05 16:44:30 +0900

    [FE] 수정 : 수정
    Former-commit-id: 1901f4f2e7fed28ae7de3b5cd36643fab96b640d

--- front-end/src/landing/components/LoginHandler.js
+++ front-end/src/landing/components/LoginHandler.js
@@ -70,7 +70,7 @@ const LoginHandler = (props) => {
               } else {
                 // jwt 토큰과 유저 이름을 저장 후 메인 페이지로 이동
                 // 메인 페이지 이동 시 선수 전체 데이터와 유저 데이터를 요청 및 저장
-                dispatch(playersActions.getPlayerAPI());
+                dispatch(playersActions.getPlayerAPI(res.data.jwt));
                 dispatch(userActions.setJwt(res.data.jwt));
                 dispatch(userActions.setLoginType(""));
                 dispatch(usersActions.getUserAPI(res.data.jwt));
