4c458aac1e90fe578847894c1b90923601dd983c
Juwon-Kiim <sa6812000@naver.com>
2022-10-06 20:04:55 +0900

    [BE] 수정 : Description 엔티티 수정

--- back-end/src/main/java/com/ssafy/theground/entity/Description.java
+++ back-end/src/main/java/com/ssafy/theground/entity/Description.java
@@ -26,92 +26,74 @@ public class Description {
     @JoinColumn(name="match_seq")
     private Match match;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_1st_top")
     private String description1stTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_1st_bottom")
     private String description1stBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_2nd_top")
     private String description2ndTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_2nd_bottom")
     private String description2ndBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_3rd_top")
     private String description3rdTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_3rd_bottom")
     private String description3rdBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_4th_top")
     private String description4thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_4th_bottom")
     private String description4thBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_5th_top")
     private String description5thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_5th_bottom")
     private String description5thBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_6th_top")
     private String description6thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_6th_bottom")
     private String description6thBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_7th_top")
     private String description7thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_7th_bottom")
     private String description7thBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_8th_top")
     private String description8thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_8th_bottom")
     private String description8thBottom;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_9th_top")
     private String description9thTop;
 
-    @NotNull
     @ColumnDefault("")
     @Column(name="description_9th_bottom")
     private String description9thBottom;
