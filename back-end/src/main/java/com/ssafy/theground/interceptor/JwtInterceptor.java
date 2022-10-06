d8b6da2407ef3a0106e0842268792c791a5b6b31
Devillness <78412140+Devillness@users.noreply.github.com>
2022-10-06 12:42:29 +0900

    [BE] 수정 : JwtInterceptor 수정

--- back-end/src/main/java/com/ssafy/theground/interceptor/JwtInterceptor.java
+++ back-end/src/main/java/com/ssafy/theground/interceptor/JwtInterceptor.java
@@ -1,22 +1,19 @@
 package com.ssafy.theground.interceptor;
 
+import com.ssafy.theground.service.JwtServiceImpl;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import com.ssafy.theground.service.JwtServiceImpl;
-
-@Component
 public class JwtInterceptor implements HandlerInterceptor {
-
 	@Resource
 	private JwtServiceImpl jwtService;
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+	public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
 		String jwt = request.getHeader("X-ACCESS-TOKEN");
 //		String jwt = request.getHeader("Authorization");
 		if (jwt != null && jwtService.isUsable(jwt)) {
