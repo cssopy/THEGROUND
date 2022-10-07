aad8851e1e04fe25aa6c3fd13ddb33ed9069043c
cssopy <83584721+cssopy@users.noreply.github.com>
2022-10-07 11:13:49 +0900

    기타 수정
    Former-commit-id: d9f67c2e8b2b4bc31a135c208139d4a6c0132f7c

--- front-end/node_modules/@nivo/radar/dist/types/Radar.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { RadarSvgProps } from './types';
-export declare const Radar: <D extends Record<string, unknown>>({ isInteractive, animate, motionConfig, theme, renderWrapper, ...otherProps }: RadarSvgProps<D>) => JSX.Element;
-//# sourceMappingURL=Radar.d.ts.map
--- front-end/node_modules/@nivo/radar/dist/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './Radar';
-export * from './ResponsiveRadar';
-export * from './RadarDots';
-export * from './props';
-export * from './types';
-//# sourceMappingURL=index.d.ts.map
