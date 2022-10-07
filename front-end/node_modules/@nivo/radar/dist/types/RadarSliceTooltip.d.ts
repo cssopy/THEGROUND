85b52fecc0d171d2494a2591f072d42978648e40
cssopy <83584721+cssopy@users.noreply.github.com>
2022-10-07 11:13:49 +0900

    기타 수정
    Former-commit-id: 47f4efba4d3fd0cfc5494f0f11efe335be70d6d2

--- front-end/node_modules/@nivo/radar/dist/types/RadarSliceTooltip.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { RadarSliceTooltipProps } from './types';
-export declare const RadarSliceTooltip: ({ index, data }: RadarSliceTooltipProps) => JSX.Element;
-//# sourceMappingURL=RadarSliceTooltip.d.ts.map
